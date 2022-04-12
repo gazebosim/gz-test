3b1224f0d2125da5a0ce8dc54c981b826be7a1d2
Nate Koenig <nate@openrobotics.org>
2022-04-12 14:09:22 -0700

    updated build script
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -2,18 +2,18 @@
 
 ./scripts/build_ign.sh ignitionrobotics ign-cmake ign-cmake2
 ./scripts/build_ign.sh ignitionrobotics ign-tools ign-tools1
-./scripts/build_ign.sh ignitionrobotics ign-math ign-math6
+./scripts/build_ign.sh ignitionrobotics ign-math is_time_helper
 ./scripts/build_ign.sh ignitionrobotics ign-utils ign-utils1
 ./scripts/build_ign.sh ignitionrobotics ign-common ign-common4
 ./scripts/build_ign.sh ignitionrobotics ign-msgs ign-msgs8
 ./scripts/build_ign.sh ignitionrobotics ign-transport ign-transport11
 ./scripts/build_ign.sh ignitionrobotics ign-fuel-tools ign-fuel-tools7
-./scripts/build_ign.sh ignitionrobotics sdformat sdf12
+./scripts/build_ign.sh ignitionrobotics sdformat plugin_equality
 ./scripts/build_ign.sh ignitionrobotics ign-plugin ign-plugin1
 ./scripts/build_ign.sh ignitionrobotics ign-physics ign-physics5
 ./scripts/build_ign.sh ignitionrobotics ign-rendering ign-rendering6
 ./scripts/build_ign.sh ignitionrobotics ign-gui ign-gui6 
 ./scripts/build_ign.sh ignitionrobotics ign-sensors ign-sensors6
-./scripts/build_ign.sh ignitionrobotics ign-gazebo use_sdf_plugin 
+./scripts/build_ign.sh ignitionrobotics ign-gazebo sensor_plugin_autoload
 # ./scripts/build_ign.sh ignitionrobotics ign-launch ign-launch5
 # ./scripts/build_ign.sh nkoenig ign-test main
